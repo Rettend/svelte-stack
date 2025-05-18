760f154b38be655e6c0fd63cef186ffb51573e16
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2025-05-18 18:28:12 +0200

    fix: e2e tests now run

--- test/e2e/home.test.ts
+++ test/e2e/home.test.ts
@@ -3,6 +3,6 @@ import { expect, test } from './test-options'
 test.describe('Home Page', () => {
   test('should display the home page and a heading', async ({ page }) => {
     await page.goto('/')
-    await expect(page.locator('h1').first()).toBeVisible()
+    await expect(page.locator('h1:text(\'Svelte Stack\')')).toBeVisible()
   })
 })
--- test/e2e/todos.test.ts
+++ test/e2e/todos.test.ts
@@ -3,6 +3,8 @@ import { expect, test } from './test-options'
 test.describe('Todos Page', () => {
   test('should display the todos page and a heading', async ({ page }) => {
     await page.goto('/todos')
-    await expect(page.locator('h1', { hasText: 'My Todos' }).first()).toBeVisible()
+    const todosPageContainer = page.locator('div.container:has(h1:text(\'My Todos\'))')
+    await expect(todosPageContainer).toBeVisible()
+    await expect(todosPageContainer.locator('h1:text(\'My Todos\')')).toBeVisible()
   })
 })
