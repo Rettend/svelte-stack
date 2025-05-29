eaa2d3fef85c634e3a7cc1f60975ca03de876512
Hegyi Áron Ferenc <36823200+Rettend@users.noreply.github.com>
2025-05-29 14:34:27 +0200

    fix: db in r:preview

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,15 +1,20 @@
-import { dev } from '$app/environment'
 import { env } from '$env/dynamic/private'
 import { createClient } from '@libsql/client'
 import { drizzle } from 'drizzle-orm/libsql'
 import * as schema from './schema'
 
-if (!env.DATABASE_URL_LOCAL)
-  throw new Error('DATABASE_URL_LOCAL is not set')
-if (!env.DATABASE_URL_REMOTE)
-  throw new Error('DATABASE_URL_REMOTE is not set')
-if (!dev && !env.DATABASE_AUTH_TOKEN)
-  throw new Error('DATABASE_AUTH_TOKEN is not set')
+const dev = env.NODE_ENV === 'development'
+
+if (dev) {
+  if (!env.DATABASE_URL_LOCAL)
+    throw new Error('DATABASE_URL_LOCAL is not set for development')
+}
+else {
+  if (!env.DATABASE_URL_REMOTE)
+    throw new Error('DATABASE_URL_REMOTE is not set for production')
+  if (!env.DATABASE_AUTH_TOKEN)
+    throw new Error('DATABASE_AUTH_TOKEN is not set for production')
+}
 
 const client = createClient({
   url: dev ? env.DATABASE_URL_LOCAL : env.DATABASE_URL_REMOTE,
